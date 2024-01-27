223f4824b868d0df7adf4ff9c5fcd4051d549a6b
mildobread <mildobread@gmail.com>
2024-01-28 02:22:20 +0900

    react study 2
    Signed-off-by: mildobread <mildobread@gmail.com>

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -5,19 +7,30 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']); // Destructuring
-  let [logo, setLogo] = useState('ReactBlog');
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']); // Destructuring
+  let [따봉, 따봉변경] = useState(0);
+
+  function 함수() {
+    let 새글제목 = [...글제목];
+    새글제목[0] = '남자 코트 추천';
+    글제목변경(새글제목);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <h4 style={{color: 'red', fontSize: '25px'}}>'ReactBlog'</h4>
       </div>
       <div className='list'>
-        <h4>{글제목[0]}</h4>
+        <h4>{글제목[0]}
+          <span onClick={()=>따봉변경(따봉+1)}>👍</span>{따봉}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{글제목[1]}</h4>
+        <h4>{글제목[1]}
+          <span onClick={()=>함수()}>👩</span>
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
