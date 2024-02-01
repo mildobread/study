43d8a2a20344427c9ea219d3977cb6cf1528f212
mildobread <mildobread@gmail.com>
2024-02-02 02:40:33 +0900

    props 2
    Signed-off-by: mildobread <mildobread@gmail.com>

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -6,41 +6,69 @@ import { useState } from 'react';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']); // Destructuring
+  let [글제목, 글제목변경] = useState(['게시글1', '게시글2', '게시글3']); // Destructuring
   let [따봉들, 따봉들변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
   let [title, setTitle] = useState(0);
-
-  function 함수() {
-    let copy = [...글제목];
-    copy[0] = '여자 코트 추천';
-    글제목변경(copy);
-  }
-
+  let [입력값, 입력값변경] = useState('');
+  let [날짜들, 날짜들변경] = useState(['1월 30일 발행', '1월 30일 발행', '1월 30일 발행']);
+  
   function 정렬() {
     let copy = [...글제목];
     copy.sort();
-    글제목변경(copy)
+    글제목변경(copy);
   }
 
   function 따봉변경(i) {
     let copy = [...따봉들];
     copy[i] = copy[i] + 1;
-    따봉들변경(copy)
+    따봉들변경(copy);
   }
 
   function 모달변경(i) {
-    setTitle(i)
-    setModal(true)
+    setTitle(i);
+    setModal(true);
+  }
+
+  function 글추가(입력값) {
+    if (!입력값) return;
+    let copy = [...글제목];
+    copy.unshift(입력값);
+    글제목변경(copy);
+
+    copy = [...따봉들];
+    copy.unshift(0);
+    따봉들변경(copy);
+  
+    copy = [...날짜들];
+    copy.unshift(날짜());
+    날짜들변경(copy);
+  }
+
+  function 날짜() {
+    let currentDate = new Date();
+    let currentMonth = currentDate.getMonth() + 1;
+    let currentDay = currentDate.getDate();
+    let currentTimeString = currentDate.toLocaleTimeString();
+    return currentMonth + '월 ' + currentDay + '일 발행 - ' + currentTimeString;
+  }
+
+  function 글삭제(i) {
+    let copy = [...글제목];
+    copy.splice(i, 1)
+    글제목변경(copy);
+
+    copy = [...따봉들];
+    copy.splice(i, 1)
+    따봉들변경(copy);
   }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color: 'white', fontSize: '25px'}}>'ReactBlog'</h4>
+        <h4 style={{color: 'white', fontSize: '25px'}}>ReactBlog</h4>
       </div>
       <button onClick={()=>정렬()}>가나다순정렬👩</button>
-      <button onClick={()=>함수()}>글수정👩</button>
       {
         글제목.map(function(a, i){
           return (
@@ -49,13 +77,18 @@ function App() {
                 <span onClick={()=>모달변경(i)}>{a}</span>
                 <span onClick={()=>따봉변경(i)}> 👍</span>{따봉들[i]}
               </h4>
-              <p>2월 17일 발행</p>
+              <p>{날짜들[i]}</p>
+              <button onClick={()=>글삭제(i)}>삭제</button>
             </div>
           )
         })
       }
+      <input onChange={(e)=>{
+        입력값변경(e.target.value)
+      }}/>
+      <button onClick={()=>글추가(입력값)}>글쓰기</button>
       {
-        modal == true ? <Modal 글제목={글제목[title]}></Modal> : null
+        modal == true ? <Modal 글제목={글제목[title]}/> : null
       }
     </div>
   );
