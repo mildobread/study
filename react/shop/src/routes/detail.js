aa1bd311b941f14c8a3f2be5c2970d270253aaf7
mildobread <mildobread@gmail.com>
2024-02-27 00:36:58 +0900

    Add Tabs
    Signed-off-by: mildobread <mildobread@gmail.com>

--- react/shop/src/routes/detail.js
+++ react/shop/src/routes/detail.js
@@ -1,16 +1,15 @@
 import { Col, Container, Row } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 
 function Detail(props) { // Component
-  
-  let [count, setCount] = useState(0);
-  let [discount, setAlert] = useState(true);
 
+  let [discount, setAlert] = useState(true);
+  let [tab, setTab] = useState(0);
   let { id } = useParams();
   let shoe = props.shoes.find(x => x.id === parseInt(id));
-  let [inputValue, setInput] = useState('');
 
   useEffect(() => {
     let timer = setTimeout(() => { setAlert(false) }, 2000)
@@ -20,37 +19,50 @@ function Detail(props) { // Component
     }
   }) // }, [])는 Dependency
 
-  useEffect(() => {
-    console.log(inputValue)
-    if (isNaN(inputValue)) {
-      alert("그러지마세요");
-    }
-  }, [inputValue])
-
   return (
     <Container>
       {
-        discount == true
+        discount === true
         ? <div className="alert alert-warning">
             2초이내 구매시 할인
           </div> : null
       }
-      {count}
-      <button onClick={()=>{ setCount(count + 1)}}>버튼</button>
-      <Row>
-        <Col>
-          <img src={"https://raw.githubusercontent.com/mildobread/study/main/react/shop/public/img/shoes" + (parseInt(id) + 1) + ".jpg"} width="80%" />
-          <input style={{ display: "block", margin: "0 auto" }} onChange={(e)=>{
-            setInput(e.target.value);
-          }}/>
+      <Row style={{ alignItems: "center" }}>
+        <Col className="col-md-6">
+          <img src={"https://raw.githubusercontent.com/mildobread/study/main/react/shop/public/img/shoes" + (parseInt(id) + 1) + ".jpg"} width="80%" alt="shoe img"/>
+        </Col>
+        <Col className="col-md-6">
           <h4 className='pt-3'>{shoe.title}</h4>
           <p>{shoe.content}</p>
           <p>{shoe.price}</p>
           <button className="btn btn-danger">주문하기</button>
         </Col>
       </Row>
+
+      <Nav variant="tabs" defaultActiveKey="버튼0">
+        <Nav.Item>
+          <Nav.Link onClick={()=>setTab(0)} eventKey="버튼0">버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={()=>setTab(1)} eventKey="버튼1">버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={()=>setTab(2)} eventKey="버튼2">버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent tab={tab} />
     </Container>
   )
+}
+
+function TabContent({tab}) {
+  if  (tab === 0) {
+    return <div>내용0</div>
+  } else if  (tab === 1) {
+    return <div>내용1</div>
+  } else if  (tab === 2) {
+    return <div>내용2</div>
+  }
 }
 
 export default Detail;
